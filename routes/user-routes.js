aadce3ce44ed52fd41bc8f456cb08c64611b5479
kishan-pj <bhootk123@gmail.com>
2022-12-16 09:10:52 +0545

    user update

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,41 +1,135 @@
-const { hash } = require('bcrypt')
-const express=require('express')
-const { model } = require('mongoose')
-const User = require('../models/User')
-const { use } = require('./books-router')
+const express = require("express");
 
-const bcrypt=express.Router('bcryptjs')
+const User = require("../models/User");
 
-const router =express.Router()
+const router = express.Router();
 
-router.post('/register',(req,res,next)=>{
-    User.findOne({username:req.body.username}
-        .then(user =>{
-            if(user!=bull){
-                let err =new Error(`User${req.body.username}already exists.`)
-                return next(new(err))
+const bcrypt = require("bcryptjs");
+
+const jwt = require('jsonwebtoken')
+
+
+
+router.post("/register", (req, res, next) => {
+
+    User.findOne({ username: req.body.username })
+
+        .then((user) => {
+
+            if (user != null) {
+
+                let err = new Error(`user ${req.body.username} already exists`);
+
+                return next(err);
+
+            } else {
+
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+
+                    if (err) {
+
+                        return next(err);
+
+                    } else {
+
+                        user = new User();
+
+                        (user.username = req.body.username), (user.password = hash);
+
+                        user.save().then((user) => {
+
+                            res
+
+                                .status(201)
+
+                                .json({
+
+                                    reply: "User Registered Sucessfully",
+
+                                    userId: user._id,
+
+                                    username: user.username,
+
+                                });
+
+                        });
+
+                    }
+
+                });
+
             }
-            bcrypt.hash(req.body.password,10,(err,hash)=>{
-                if(err) return next(err)
-                let user=new User()
-                user.username=req.body.username
-                user.password=hash
-                user.save()
-                .then(user=>{
-                    res.status(201).json({
-                        'status':'User registered sucessfully',
-                        userId:user_id,
-                        username:user.username
-                    })
-                }).catch(next)
-            })
-        })).catch(next)
+
+        })
+
+        .catch(next);
+
+});
 
 
 
-})
-router.post('/login',(req,res,next)=>{
-    res.send("Login request")
+router.post("/login", (req, res, next) => {
 
-})
-module.exports=router
+    User.findOne({ username: req.body.username })
+
+        .then(user => {
+
+            if (user == null) {
+
+                let err = new Error(`User ${req.body.username} has not registered`)
+
+                return next(err)
+
+            }
+
+            bcrypt.compare(req.body.password, user.password,
+
+                (err, status) => {
+
+                    if (err) return next(err)
+
+                    if (!status) {
+
+                        let err = new Error('Password doesnot match')
+
+                        return next(err)
+
+                    }
+
+
+
+                    let data = {
+
+                        userId: user._id,
+
+                        username: user.username
+
+                    }
+
+                    jwt.sign(data, process.env.SECRET,
+
+                        { 'expiresIn': '1d' }, (err, token) => {
+
+                            if (err) return next(err)
+
+                            res.json({
+
+                                status: 'Login Sucessful',
+
+                                token: token
+
+                            })
+
+                        })
+
+
+
+
+
+                })
+
+        }).catch(next)
+
+});
+
+module.exports = router;
